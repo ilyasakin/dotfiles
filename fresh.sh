5f73ffe466a784de9f0cc9e6e6b0b00d5e6e231b
İlyas Akın <ashnwor@gmail.com>
2023-03-08 21:34:00 +0300

    Make sure timer and terminal-notifier are installed

--- fresh.sh
+++ fresh.sh
@@ -5,30 +5,39 @@ echo "Kickstarting this baby"
 # Utils
 
 isOhMyZshExists() {
-  [ -d ~/.oh-my-zsh ]
+	[ -d ~/.oh-my-zsh ]
 }
 
 installOhMyZsh() {
-  /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
+	/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/HEAD/tools/install.sh)"
 }
 
 installHomebrew() {
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >>$HOME/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv)"
 }
 
 isCommandExists() {
-  command -v "$1" &> /dev/null
+	command -v "$1" &
+	>/dev/null
 }
 
 checkIfFileExists() {
-    [ -f "$1" ]
+	[ -f "$1" ]
 }
 
 installAntigen() {
-    curl -L git.io/antigen > antigen.zsh
+	curl -L git.io/antigen >antigen.zsh
+}
+
+installTimer() {
+	brew install caarlos0/tap/timer
+}
+
+installTerminalNotifier() {
+	brew install terminal-notifier
 }
 
 # Setup
@@ -36,19 +45,19 @@ installAntigen() {
 if isOhMyZshExists; then
 	echo "oh-my-zsh is installed"
 else
-  installOhMyZsh
+	installOhMyZsh
 fi
 
 if isCommandExists brew; then
-  echo "homebrew is installed"
+	echo "homebrew is installed"
 else
-  installHomebrew
+	installHomebrew
 fi
 
 if checkIfFileExists "./antigen.zsh"; then
-    echo "antigen is installed"
+	echo "antigen is installed"
 else
-    installAntigen
+	installAntigen
 fi
 
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
@@ -59,9 +68,16 @@ rm -rf ~/.config/kitty
 mkdir ~/.config/kitty
 ln -sf ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 
+if isCommandExists timer; then
+	echo "timer is installed"
+else
+	installTimer
+fi
 
-
-
-
+if isCommandExists terminal-notifier; then
+	echo "terminal-notifier is installed"
+else
+	installTerminalNotifier
+fi
 
 echo "Finished kickstarting"
