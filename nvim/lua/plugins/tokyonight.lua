eb275e7d26c17a2ae75209dec43eaa049202dc36
İlyas Akın <ashnwor@gmail.com>
2023-03-13 05:02:40 +0300

    Make tokyonight transparent

---
+++ nvim/lua/plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+local M = {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
+
+return M
