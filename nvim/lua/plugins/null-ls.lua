6176bbe06254eb326d17944d73343d1016c2e747
İlyas Akın <ashnwor@gmail.com>
2023-06-26 08:27:57 +0300

    Load eslint conditionally

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -1,3 +1,30 @@
+local function file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+local function eslint_config_exists()
+  local eslintrc = vim.fn.glob(".eslintrc*", false, true)
+
+  if not vim.tbl_isempty(eslintrc) then
+    return true
+  end
+
+  -- TODO: This should be happening in the root of the project no matter where it opened
+  if file_exists("package.json") and vim.fn.filereadable("package.json") then
+    if vim.fn.json_decode(vim.fn.readfile("package.json"))["eslintConfig"] then
+      return true
+    end
+  end
+
+  return false
+end
+
 local M = {
   "jose-elias-alvarez/null-ls.nvim",
   event = "BufReadPre",
@@ -9,20 +36,37 @@ local M = {
     local completion = null_ls.builtins.completion
     local lsp_utils = require("lspconfig.util")
 
-    return {
-      debug = false,
-      sources = {
-        formatting.shfmt,
-        formatting.stylua,
-        formatting.prettierd,
+    local sources = {
+      formatting.shfmt,
+      formatting.stylua,
+      formatting.prettierd,
+      code_actions.gitsigns,
+    }
+
+    if eslint_config_exists() then
+      table.insert(
+        sources,
         diagnostics.eslint_d.with({
           command = "eslint_d",
           cwd = function(params)
             return lsp_utils.root_pattern(".git")(params.bufname)
           end,
-        }),
-        code_actions.gitsigns,
-      },
+          config = {
+            root_dir = function()
+              if not eslint_config_exists() then
+                return nil
+              end
+
+              return vim.fn.getcwd()
+            end,
+          },
+        })
+      )
+    end
+
+    return {
+      debug = false,
+      sources = sources,
     }
   end,
 }
