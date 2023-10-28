c8b22d01c1b2af3a1b49a559cb11b9e83b96155f
İlyas Akın <ashnwor@gmail.com>
2023-10-29 00:07:15 +0300

    Fix ghost links

--- fresh.sh
+++ fresh.sh
@@ -60,14 +60,21 @@ else
 	installAntigen
 fi
 
+unlink ~/.ideavimrc
 ln -sf ~/dotfiles/.ideavimrc ~/.ideavimrc
+unlink ~/.tmux.conf
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+unlink ~/.config/yabai
 ln -sf ~/dotfiles/yabai ~/.config/yabai
+unlink ~/.config/skhd
 ln -sf ~/dotfiles/skhd ~/.config/skhd
+unlink ~/.config/tmux
 ln -sf ~/dotfiles/tmux ~/.config/tmux
+unlink ~/.config/alacritty
 ln -sf ~/dotfiles/alacritty ~/.config/alacritty
 
 # Kitty
+unlink ~/.config/kitty
 rm -rf ~/.config/kitty
 mkdir ~/.config/kitty
 ln -sf ~/dotfiles/kitty.conf ~/.config/kitty/kitty.conf
